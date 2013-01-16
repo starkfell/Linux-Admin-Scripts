93802f4164039a5727260afff8c05144b78c2205
Ryan Irujo <ryan.irujo@gmail.com>
2013-01-15 22:33:40 -0500

    Update remote_host_info_query.pl
    - Perl ReadKey Module Added.
    - Configured Expect to log STDOUT and set the log file as the Query Text File
      declared at the beginning of the script.

--- remote_host_info_query.pl
+++ remote_host_info_query.pl
@@ -6,46 +6,62 @@
 # Inception:     01.14.2013
 # Last Modified: 01.15.2013
 #
-# Description:   Script that queries the IP Address and OS Type on a set of hosts (read from a text file). Simply type in the name
-#                of all of the Hosts you would like to query in same directory as this script is in and make sure
-#                that it is named 'hosts.txt'. Additionally, you can rename the text file by specifying a different
-#                name in the '$host_list' variable below. Note that using Syntax 1 cause less noise to appear in the
-#                Terminal Window, but provides the exact same results as Syntax 2.
+# Description:   Script that queries the IP Address and OS Type on a set of hosts (read from a text file).
+#                Add of all of the Hosts you would like to query in listed column name the file 'hosts.txt'.
+#                You can rename the text file by specifying a different  name in the '$host_list' variable below.
 #
 #
-# Changes:
+# Changes:       1.15.2012 - [R. Irujo]
+#                - Added ReadKey Module in order to hide username and password credentials passed to the script
+#                  from the terminal.
 #
 #
-# Syntax 1:      ./remote_host_info_query [Username] [Password] > [ip_os_query_file]
+# Syntax 1:      ./remote_host_info_query [Username] [Password]
 #
-# Command Line:  ./remote_host_info_query username P@wer89d! > ip_os_query.txt
-
+# Command Line:  ./remote_host_info_query username P@wer89d!
 
 use warnings;
 use Expect;
+use Term::ReadKey;
+
 
 #--------------------------------------------------------------
 # Main Variables
 #--------------------------------------------------------------
 
+
 my $ssh                    = "/usr/bin/ssh";
 my $host_list              = "./hosts.txt";
-my $user                   = $ARGV[0];
-my $password               = $ARGV[1];
-my $timeout                = 10;
 my $ip_os_query_file       = "./ip_os_query.txt";
 my $ip_os_results_file     = "./ip_os_results.txt";
+my $timeout                = 10;
 
 
-if (!defined $user || $user eq ""){
+# Gathering Credentials used to login to the Remote Hosts.
+ReadMode('noecho');
+
+print "Please provide a username to login to the Hosts being queried:\n";
+chomp(my $username = <>);
+
+print "Please provide a password:\n";
+chomp(my $password = <>);
+
+ReadMode('normal');
+
+
+# Making sure Credentials are not empty.
+if (!defined $username || $username eq ""){
         print "A [Username] must be provided.\n";exit 3;
         }
 if (!defined $password || $password eq ""){
         print "A [Password] must be provided.\n";exit 3;
         }
 
+
+# Adding/Preparing both the Query and Results Text Files.
 if (-e $ip_os_query_file) {
-        print "$ip_os_query_file file already exists.\n";
+        system(`rm ./$ip_os_query_file`);
+        print "$ip_os_query_file found. Clearing out previous results.\n";
         }
 else {
         system(`touch $ip_os_query_file`);
@@ -53,7 +69,8 @@ else {
         }
 
 if (-e $ip_os_results_file) {
-        print "$ip_os_results_file already exists.\n";
+        system(`rm ./$ip_os_results_file`);
+        print "$ip_os_results_file found. Clearing out previous results.\n";
         }
 else {
         system(`touch $ip_os_results_file`);
@@ -68,7 +85,7 @@ else {
 
 foreach $server (`cat $host_list`){
         chomp($server);
-        &ip_os_remote_query ($user, $password, $server, $timeout);
+        &ip_os_remote_query ($username, $password, $server, $timeout);
 }
 
 my $final_results = system(`cat $ip_os_query_file | grep ">" | cut -d'>' -f2 | cut -c 2-300 > $ip_os_results_file`);
@@ -76,11 +93,11 @@ my $results_check = `cat $ip_os_results_file | wc -l`;
 
 
 if ($results_check < 1 ) {
-        warn "There were NO Results written to the [ip_os_results.txt] file.\n";
+        warn "\nThere were NO Results written to the [ip_os_results.txt] file.\n";
         exit 0;
 }
 elsif ($results_check >= 1 ) {
-        warn "Final Results are available in the [ip_os_results.txt] file.\n";
+        warn "\nFinal Results are available in the [ip_os_results.txt] file.\n";
         exit 2;
 }
 
@@ -92,7 +109,7 @@ elsif ($results_check >= 1 ) {
 
 sub ip_os_remote_query (){
 
-my $user      = shift;
+my $username  = shift;
 my $password  = shift;
 my $server    = shift;
 my $timeout   = shift;
@@ -107,8 +124,14 @@ my $prompt    = '\$\s*';
         $stats_exp->raw_pty(1);
 
 
+        # Enabling STDOUT Logging which will redirect the results of the script to the 'ip_os_query_file'
+        # for additional parsing later.
+        $stats_exp->log_stdout(1);
+        $stats_exp->log_file($ip_os_query_file);
+
+
         # Spawning SSH Session to Remote Host.
-        $stats_exp->spawn("$ssh $user\@$server") or die "Cannot spawn ssh: $!\n";
+        $stats_exp->spawn("$ssh $username\@$server") or die "Cannot spawn ssh: $!\n";
 
         # Running Expect Function.
         $stats_exp->expect($timeout,
@@ -143,7 +166,7 @@ my $prompt    = '\$\s*';
         # the Script keeps running for the the rest of the Servers in the Hosts.txt file.
 
         [qr'Connection closed by remote host'  => sub {warn "SSH Session Forcefully Closed by $server.\n";}],
-        [qr'incident will be reported'         => sub {warn "$user does not have sudo rights on $server.\n";}],
+        [qr'incident will be reported'         => sub {warn "$username does not have sudo rights on $server.\n";}],
         [qr'IOError\s*'                        => sub {warn "Something went wrong while querying $server.\n";}],
         [qr'OSError\s*'                        => sub {warn "Something went wrong while querying $server.\n";}],
         [EOF                                   => sub {warn "Error: Could not login!\n"; }],
@@ -159,3 +182,4 @@ my $prompt    = '\$\s*';
 #--------------------------------------------------------------
 # END!
 #--------------------------------------------------------------
+
