2ad3036898da4f5956c93540a4466f82a5b4877a
Ryan Irujo <ryan.irujo@gmail.com>
2013-01-17 13:50:11 -0500

    Update remote_host_info_query.pl

--- remote_host_info_query.pl
+++ remote_host_info_query.pl
@@ -1,15 +1,15 @@
 #!/usr/bin/perl
 #
-#  --- [remote_host_info_query] Perl Script  ---
+#  --- [remote_server_info_query] Perl Script  ---
 #
 # Author(s):     Ryan Irujo
 # Inception:     01.14.2013
-# Last Modified: 01.16.2013
+# Last Modified: 01.17.2013
 #
-# Description:   Script that queries the IP Address and OS Type on a set of hosts (read from a text file).
-#                Add of all of the Hosts you would like to query in listed column name the file 'hosts.txt'.
-#                You can rename the text file by specifying a different  name in the '$host_list' variable below.
-#                The Script will prompt for a username and password to use for logging into the remote hosts
+# Description:   Script that queries the IP Address and OS Type on a set of Servers (read from a text file).
+#                Add of all of the Servers you would like to query into a single column in a file named 'servers.txt'.
+#                You can rename the text file by specifying a different name in the '$server_list' variable below.
+#                The Script will prompt for a username and password to use for logging into the remote Servers
 #                as soon as you run it.
 #
 #
@@ -18,31 +18,34 @@
 #                  from the terminal.
 #
 #
-# Syntax:        ./remote_host_info_query
+# Syntax:        ./remote_server_info_query
 #
-# Command Line:  ./remote_host_info_query
+# Command Line:  ./remote_server_info_query
 
 use warnings;
 use Expect;
 use Term::ReadKey;
 
 
-#--------------------------------------------------------------
+#--------------------
 # Main Variables
-#--------------------------------------------------------------
+#--------------------
 
 
 my $ssh                    = "/usr/bin/ssh";
-my $host_list              = "./hosts.txt";
-my $ip_os_query_file       = "./ip_os_query.txt";
-my $ip_os_results_file     = "./ip_os_results.txt";
+my $server_list            = "servers.txt";
+my $query_file             = "ip_os_query.txt";
+my $results_file           = "ip_os_results.txt";
 my $timeout                = 10;
 
 
-# Gathering Credentials used to login to the Remote Hosts.
+#----------------------------------------------------------------
+# Gathering Credentials used to login to the Remote Servers.
+#----------------------------------------------------------------
+
 ReadMode('noecho');
 
-print "Please provide a username to login to the Hosts being queried:\n";
+print "Please provide a username:\n";
 chomp(my $username = <STDIN>);
 
 print "Please provide a password:\n";
@@ -51,38 +54,81 @@ chomp(my $password = <STDIN>);
 ReadMode('normal');
 
 
+#--------------------------------------------
 # Making sure Credentials are not empty.
+#--------------------------------------------
+
 if (!defined $username || $username eq ""){
-        print "A [Username] must be provided.\n";exit 3;
+        print "\nA [Username] must be provided.\n";exit 2;
         }
 if (!defined $password || $password eq ""){
-        print "A [Password] must be provided.\n";exit 3;
+        print "\nA [Password] must be provided.\n";exit 2;
         }
 
 
-# Adding/Preparing both the Query and Results Text Files.
-if (-e $ip_os_query_file) {
-        system("rm ./$ip_os_query_file");
-        print "$ip_os_query_file found. Clearing out previous results.\n";
+#--------------------------------------
+# Adding/Recreating the Query File
+#--------------------------------------
+
+if (-e $query_file) {
+        system("rm ./$query_file");
+        if ($? == 0) {
+                print "\n$query_file found. Deleting it.\n";
+                system("touch ./$query_file");
+                if ($? == 0) {
+                        print "New $query_file created successfully.\n";
+                }
+                elsif ($? != 0) {
+                print "There was a problem creating the $query_file\n";
+                exit 2;
+                }
         }
+}
 else {
-        system("touch $ip_os_query_file");
-        print "$ip_os_query_file file created.\n";
+        system("touch ./$query_file");
+        if ($? == 0) {
+                print "\nNew $query_file created successfully.\n";
         }
+        elsif ($? != 0) {
+                print "There was a problem creating the $query_file.\n";
+                exit 2;
+        }
+}
 
-if (-e $ip_os_results_file) {
-        system("rm ./$ip_os_results_file");
-        print "$ip_os_results_file found. Clearing out previous results.\n\n";
+
+#----------------------------------------
+# Adding/Recreating the Results File
+#----------------------------------------
+
+if (-e $results_file) {
+        system("rm ./$results_file");
+        if ($? == 0) {
+                print "\n$results_file found. Deleting it.\n";
+                system("touch ./$results_file");
+                if ($? == 0) {
+                        print "New $results_file created successfully.\n\n";
+                }
+                elsif ($? != 0) {
+                print "There was a problem creating the $results_file\n";
+                exit 2;
+                }
         }
+}
 else {
-        system("touch $ip_os_results_file");
-        print "$ip_os_results_file file created.\n\n";
+        system("touch ./$results_file");
+        if ($? == 0) {
+                print "New $results_file created successfully.\n\n";
         }
+        elsif ($? != 0) {
+                print "There was a problem creating the $results_file.\n";
+                exit 2;
+        }
+}
 
 
-#------------------------------------------------------------------------
-# Verifying Host List is Available and User wants to Execute Script
-#------------------------------------------------------------------------
+#----------------------------------------------------------------------------
+# Verifying Server List is Available and User is ready to Execute Script
+#----------------------------------------------------------------------------
 
 my $choice = &verify_ready_to_run();
 
@@ -95,23 +141,22 @@ if ($choice eq "no") {
         }
 
 
-#---------------------------------------------
-# Remote Host OS Type & IP Address Check
-#---------------------------------------------
+#----------------------------------------------
+# Remote Server OS Type & IP Address Check
+#----------------------------------------------
 
-
-foreach $server (`cat $host_list`) {
+foreach $server (`cat ./$server_list`) {
         chomp($server);
         &ip_os_remote_query ($username, $password, $server, $timeout);
 }
 
 
 # Parsing out relevant entries in the Query File and formatting them in readable format in the Results File.
-my $final_results = system("cat $ip_os_query_file | grep '>' | cut -d'>' -f2 | cut -c 2-300 > $ip_os_results_file");
+my $final_results = system("cat ./$query_file | grep '>' | cut -d'>' -f2 | cut -c 2-300 > ./$results_file");
 
 
 # Verifying that the entries in the Query File were transferred to the Results File and then exiting the script.
-my $results_check = `cat $ip_os_results_file | wc -l`;
+my $results_check = `cat ./$results_file | wc -l`;
 
 
 if ($results_check < 1 ) {
@@ -124,19 +169,13 @@ elsif ($results_check >= 1 ) {
 }
 
 
-#---------------------------------------------
-# END Runtime
-#---------------------------------------------
-
-
-#---------------------------------------------
+#-----------------
 # Subroutines
-#---------------------------------------------
-
+#-----------------
 
 sub verify_ready_to_run () {
-        if (-e $host_list) {
-                system("cat $host_list\n");
+        if (-e $server_list) {
+                system("cat ./$server_list\n");
                 print "\nThe Script will now query the Servers listed above. Do you want to continue? [yes/no]\n";
                 chomp(my $choice = <STDIN>);
                 if ($choice eq "yes") {
@@ -157,7 +196,7 @@ sub verify_ready_to_run () {
                 }
         }
         else {
-                print "Check to see if [hosts.txt] is available or if the script lists a different file in the [\$host_list] variable.\n";
+                print "Unable to run as the file name listed in the [\$server_list] variable doesn't appear to exist.\n";
                 exit 2;
         }
 }
@@ -180,19 +219,19 @@ my $prompt    = '\$\s*';
         $stats_exp->raw_pty(1);
 
 
-        # Enabling STDOUT Logging which will redirect the results of the script to the 'ip_os_query_file'
+        # Enabling STDOUT Logging which will redirect the results of the script to the Query File.
         # for additional parsing later.
         $stats_exp->log_stdout(1);
-        $stats_exp->log_file($ip_os_query_file);
+        $stats_exp->log_file($query_file);
 
 
-        # Spawning SSH Session to Remote Host.
+        # Spawning SSH Session to Remote Server.
         $stats_exp->spawn("$ssh $username\@$server") or die "Cannot spawn ssh: $!\n";
 
         # Running Expect Function.
         $stats_exp->expect($timeout,
 
-        # SSH - Add Key From Remote Host Prompt.
+        # Adding SSH Key if Prompted.
         [qr'\(yes/no\)\s*'         , sub {my $exph = shift;
                                      print $exph "yes\n";
                                      exp_continue; }],
@@ -202,22 +241,26 @@ my $prompt    = '\$\s*';
                                      $action->send("$password\n");
                                      exp_continue; }],
 
-        # Query the OS Type and IP Address of the Remote Host.
+        # Query the OS Type and IP Address of the Remote Server.
         [qr'login:\s*'             , sub {my $action = shift;
                                      my $os_type = "`cat /etc/redhat-release`";
                                      my $ip_addr = "`/sbin/ifconfig | perl -nle '/dr:(\\S+)/ && print \$1' | grep -v 127.0.0.1`";
-                                     $action->send("printf \"$server;$os_type;$ip_addr\n \" ");
+                                     my $fqdn    = "`hostname`";
+                                     my $arch    = "`uname -i`";
+                                     $action->send("printf \"$fqdn;$os_type;$arch;$ip_addr\n \" ");
                                      exp_continue; }],
 
-        # Query the OS Type and IP Address of the Remote Host.
-        # This option is used if the user account running the script has never logged into the Host before.
+        # Query the OS Type and IP Address of the Remote Server.
+        # This option is used if the user account running the script has never logged into the Server before.
         [qr'Creating directory\s*' , sub {my $action = shift;
                                      my $os_type = "`cat /etc/redhat-release`";
                                      my $ip_addr = "`/sbin/ifconfig | perl -nle '/dr:(\\S+)/ && print \$1' | grep -v 127.0.0.1`";
-                                     $action->send("printf \"$server;$os_type;$ip_addr\n \" ");
+                                     my $fqdn    = "`hostname`";
+                                     my $arch    = "`uname -i`";
+                                     $action->send("printf \"$fqdn;$os_type;$arch;$ip_addr\n \" ");
                                      exp_continue; }],
 
-        # Exit out of the Remote Host.
+        # Exit out of the Remote Server.
         [$prompt                   , sub {my $action = shift;
                                      $action->send("exit\n");
                                      exp_continue; }],
@@ -227,7 +270,7 @@ my $prompt    = '\$\s*';
                                      exp_continue; }],
 
         # Exception Handling subroutines are below. All Errors are treated with 'warn' instead of 'die' to ensure that
-        # the Script keeps running for the the rest of the Servers in the Hosts.txt file.
+        # the Script keeps running for the the rest of the Servers from the '$server_list' variable.
 
         [qr'Connection closed by remote host'  => sub {warn "SSH Session Forcefully Closed by $server.\n";}],
         [qr'incident will be reported'         => sub {warn "$username does not have sudo rights on $server.\n";}],
@@ -243,6 +286,6 @@ my $prompt    = '\$\s*';
 }
 
 
-#----------------------------------------------
+#------------------
 # END of Script
-#----------------------------------------------
+#------------------
