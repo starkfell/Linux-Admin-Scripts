22d19760783b8b96d8c96d55205a1739b873d894
Ryan Irujo <ryan.irujo@gmail.com>
2014-03-24 00:04:25 +0100

    Update parse_oracle_audit_xml.pl

--- parse_oracle_audit_xml.pl
+++ parse_oracle_audit_xml.pl
@@ -13,28 +13,35 @@ use Data::Dumper;
 # Currently using the Default Path for capturing Oracle Audit XML Files: /u01/app/oracle/admin/orcl/adump/
 $AuditFile = `ls -lhtr /u01/app/oracle/admin/orcl/adump/ | tail -n 1 | perl -lane 'print \$F[8]'`;
 
-print $AuditFile;
+print "Oracle Audit File:\n";
+print $AuditFile . "\n";
 
 $RawXML_Output = `cat /u01/app/oracle/admin/orcl/adump/$AuditFile`;
 
-print $RawXML_Output;
+print "Raw XML Output:\n";
+print $RawXML_Output . "\n";
+
+
+$XML = new XML::Simple;
+
+$XML_Data = $XML->XMLin($RawXML_Output);
+
+#print "SQL Text = " . $XML_Data->{AuditRecord}->[0]->{Sql_Text} . "\n";
+
+
+foreach $Entry (@{$XML_Data->{AuditRecord}}) {
+        print "SQL Text = $Entry->{Sql_Text}\n";
+
+        $Sql_Text_Record = $Entry->{Sql_Text};
+
+        print "|$Sql_Text_Record|\n";
+
+        if ($Sql_Text_Record =~ "/Doran/") {
+                print "Matching Record Found = $Sql_Text_Record \n.";
+                exit 0;
+                }
+        }
 
 
 
-#system("cat /u01/app/oracle/admin/orcl/adump/$AuditFile");
-
-
-#$XML = new XML::Simple;
-
-
-
-#$AuditFile = $XML->XMLin("/u01/app/oracle/admin/orcl/adump/orcl_ora_2288_20140322050149732179344462.xml");
-
-#print "Shit Happens";
-#print Dumper($AuditFile);
-
-#foreach $Entry (@{$AuditFile->{Entry}}) {
-#       print $Entry->{SQL_Text} . "\n";
-#       }
-
-
+#print Dumper($XML_Data);
